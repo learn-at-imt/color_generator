3c50458506fe603704b0c4da48036aa3179ad08e
Aaron <addaeaaron28@gmail.com>
2022-05-26 22:37:02 +0000

    added css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+import { useState } from "react";
+import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(true);
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form>
+          <input
+            type="text"
+            placeholder="type color"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            Get Colors
+          </button>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default App;
+
+//import useState hook
+//the useState is used here to store the values in the input
